7860de132011fcebd8a75063eea49947c1c867ff
DVOsh <oshur96@gmail.com>
2026-08-06 21:12:01 +0300

    Удалены избыточные директвы using

--- NeuralNetwork_CodeBlog_course/Topology.cs
+++ NeuralNetwork_CodeBlog_course/Topology.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NeuralNetwork_CodeBlog_course
+﻿namespace NeuralNetwork_CodeBlog_course
 {
     public class Topology
     {
